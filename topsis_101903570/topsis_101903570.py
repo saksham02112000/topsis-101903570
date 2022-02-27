da7a5fe19303fa2977643710b96a3c5318b0f68f
Saksham Srivastava <sakshamsrini@gmail.com>
2022-02-27 23:31:08 +0530

    code_changes

--- topsis_101903570/topsis_101903570.py
+++ topsis_101903570/topsis_101903570.py
@@ -1,97 +1,148 @@
 
-import pandas as pd
-import math
-import scipy.stats as ss
+import pandas as pd 
+import numpy as np
+import os 
 import sys
-def main():
-    try:
-        file = open(sys.argv[1],'rb')
-        ww =sys.argv[2]
-        w=ww.split(",")
-        w=list(map(int, w))
-        f = sys.argv[3]
-        f=f.split(",")
-        sqr=[]
-        nm=[]
-        ds=pd.read_csv(file)
-        target=ds.iloc[:,0]
-        ds=ds.iloc[:,1:].values.astype('float64')
-        rows=ds.shape[0]
-        cols=ds.shape[1]
-        
-        for i in range(0,cols):
-            sum1=0
-            for j in range(0,rows):
-                sum1=sum1+(ds[j][i]*ds[j][i])
-            sum1=math.sqrt(sum1)
-            sqr.append(sum1)
-              
-        sum2=0 
-        for i in range(0,cols):
-            sum2=sum2+w[i]
-            
-        for i in range(0,cols):
-            w[i]=w[i] /sum2
-        
-        for i in range(0,cols):
-            for j in range(0,rows):
-                ds[j][i]=(ds[j][i]/sqr[i])*w[i]
-        
-        #f=['-','+','+','+']
-        max1=[]
-        min1=[]
-        best=[]
-        worst=[]
-        for i in range(0,cols):
-            max2=-1
-            min2=10000;
-            for j in range(0,rows):
-                if(ds[j][i]>max2):
-                    max2=ds[j][i]
-                if(ds[j][i]<min2):
-                    min2=ds[j][i]
-            if(f[i]=='+'):
-                best.append(max2)
-                worst.append(min2)
-            elif(f[i]=='-'):
-                best.append(min2)
-                worst.append(max2)
-        
-        sip=[]
-        sin=[]
-        for i in range(0,rows):
-            sumsip=0
-            sumsin=0
-            for j in range(0,cols):
-                sumsip=sumsip+(ds[i][j]-best[j])*(ds[i][j]-best[j])
-                sumsin=sumsin+(ds[i][j]-worst[j])*(ds[i][j]-worst[j])
-            sip.append(math.sqrt(sumsip))
-            sin.append(math.sqrt(sumsin))
-        p=[]
-        for i in range(0,rows):
-            p.append(sin[i]/(sip[i]+sin[i]))
-        lst=(len(p)-ss.rankdata(p)+1)
-        print("Rank of all Candidates:")
-        print(lst)
-        indexx=-1
-        j=0
-        for i in lst:
-            if( i==1):
-                indexx=j
-                break
-            j+=1
-        print("Best Candidate:")
-        print(target[indexx])
-    except:
-        if(len(sys.argv)!=4):
-            print('ERROR: Please provide four arguments')
-        elif(len(sys.argv[2])!=len(sys.argv[3])):
-            print('ERROR: Unequal length of parameters')
+
+
+
+def floater(a):  # .astype() can be used but is not reliable
+    b = []
+    for i in a:
+        try:
+            ix = []
+            for j in i:
+                ix.append(float(j))
+        except:
+            ix = float(i)
+            pass
+        b.append(ix)
+    b = np.array(b)
+    return b
+
+
+def normalize(matrix, r, n, m):
+    for j in range(m):
+        sq = np.sqrt(sum(matrix[:, j]**2))
+        for i in range(n):
+            r[i, j] = matrix[i, j]/sq
+    return r
+
+
+def weight_product(matrix, weight):
+    r = matrix*weight
+    return r
+
+
+def calc_ideal_best_worst(sign, matrix, n, m):
+    ideal_worst = []
+    ideal_best = []
+    for i in range(m):
+        if sign[i] == 1:
+            ideal_worst.append(min(matrix[:, i]))
+            ideal_best.append(max(matrix[:, i]))
         else:
-            print('ERROR')
+            ideal_worst.append(max(matrix[:, i]))
+            ideal_best.append(min(matrix[:, i]))
+    return (ideal_worst, ideal_best)
+
+
+def euclidean_distance(matrix, ideal_worst, ideal_best, n, m):
+    diw = (matrix - ideal_worst)**2
+    dib = (matrix - ideal_best)**2
+    dw = []
+    db = []
+    for i in range(n):
+        dw.append(sum(diw[i, :])**0.5)
+        db.append(sum(dib[i, :])**0.5)
+    dw = np.array(dw)
+    db = np.array(db)
+    return (dw, db)
+
+
+def performance_score(distance_best, distance_worst, n, m):
+    score = []
+    score = distance_worst/(distance_best + distance_worst)
+    return score
+
+
+def topsis(a, w, sign):
+    a = floater(a)
+    # print(a)
+    n = len(a)
+    # print(n)
+    # print(len(a[0]))
+    m = len(a[0])
+    # print('n:', n, '\nm:', m)
+    r = np.empty((n, m), np.float64)
+    r = normalize(a, r, n, m)
+    t = weight_product(r, w)
+    (ideal_worst, ideal_best) = calc_ideal_best_worst(sign, t, n, m)
+    (distance_worst, distance_best) = euclidean_distance(
+        t, ideal_worst, ideal_best, n, m)
+    score = performance_score(distance_best, distance_worst, n, m)
+    return (score)
+    # returns a tupple with index of best data point as first element and score array(numpy) as the other
+
+
 
 if __name__ == '__main__':
-    main()
+    if len(sys.argv) != 5:
+        print('Wrong Number of args')
+        print('Input should be like - \n '
+              'python <program.py> <InputDataFile> <Weights> <Impacts> <ResultFileName>]')
+    else:
+        file_path = sys.argv[1]
+        try:
+            if os.path.exists(file_path):
+                print('Path exist')
+        except OSError as err:
+            print(err.reason)
+            exit(1)
+        df = pd.read_excel(file_path)
     
-        
-        
+        if((df.dtypes[0])!=object):
+            print("first column should be object column")
+            exit(1)
+        if (len(df.columns)<3):
+            print("Too less columns")
+            exit(1)
+        newdf=df.iloc[1:,1:]
+        try:
+            newdf = newdf.apply(pd.to_numeric)
+        except OSError as error:
+            print(error.reason)
+            exit(1)
+        for i in newdf.dtypes:
+            if(i not in ['int64','float64']):
+                print("column ",i," not an integer type")
+                exit(1)
+        a = newdf.values
+        arg2 = sys.argv[2]
+        arg3 = sys.argv[3]
+        w = arg2.strip('][').split(',')
+        w = list(map(float, w))
+        s = arg3.strip('][').split(',')
+ 
+        impact=[]
+        for i in s:
+            if(i=='+'):
+                impact.append(1)
+            elif(i=='-'):
+                impact.append(-1)
+            else:
+                print("impact Signs should be + or - only ")
+                exit(1)
+
+        impact = list(map(int, impact))
+        if(len(w)!=len(impact)):
+            print("Length of weights and impackts are not equal")
+            exit(1)
+    
+        res = pd.Series(topsis(a, w, impact))
+        rank=res.rank(ascending=False)
+        a=np.array(['Topsis Score'])
+        df["Topsis Score"]=np.append(a,res)
+        p=np.array(['Rank'])
+        df["Rank"]=np.append(p,rank)
+        df.to_csv(sys.argv[4],index=False,header=False)
